90ab950129c8d688273c1e8e55cecf7a2072636a
unknown <ortolaagustin@gmail.com>
2020-04-18 21:47:40 -0300

    ex 2 resolved

--- exercises.js
+++ exercises.js
@@ -21,30 +21,32 @@ const catalog = [
 /* Desarrollar la función addToCatalog(name, stock), que recibe un nombre (texto) y un stock (cantidad), agregue ambos datos como un único objeto al array catalog. Luego tiene que mostrar por consola la colección. 
  */
 function addToCatalog(_name, _stock) {
-    // const container = {name: _name, stock: _stock}
+    // const box = {name: _name, stock: _stock}
     catalog.push({name: _name, stock: _stock})
     console.log (catalog)
 
 }
 
 /* EX1 test */
-addToCatalog('honey', 20) 
-addToCatalog('honeyvere', 25)
+//addToCatalog('honey', 20) 
+//addToCatalog('honeyvere', 25)
 
 //------------------- END EX 1 --------------------------------
 
-//------------------- EX 1 --------------------------------
+//------------------- EX 2 --------------------------------
 
-/* Ahora el cliente empieza a mandar cajas armadas, siempre de a dos a la vez. Desarrollar la función addTwoContainersToCatalog() */
+/* Ahora el cliente empieza a mandar box armadas, siempre de a dos a la vez. Desarrollar la función addTwoContainersToCatalog() */
 
-function addTwoContainersToCatalog(containers) {
-
+function addTwoContainersToCatalog(container) {
+    catalog.push(container[0], container[1])
 }
 
 /* EX2 test */
 const containers1 = [{name: 'menta', stock: 4}, {name: 'fruit', stock: 9}]
-const containers2 = [{name: 'pera', stock: 7}, {name: 'chocolate', stock: 2}]
+const containers2 = [ {name: 'pera', stock: 7}, {name: 'chocolate', stock: 2}]
 
 addTwoContainersToCatalog(containers1)
-
 addTwoContainersToCatalog(containers2)
+
+console.log(catalog)
+//------------------- EX 2 --------------------------------
