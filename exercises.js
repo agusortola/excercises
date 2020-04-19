669c203f6dc1493cdfab63504664746a5589edd6
Nicolás Ortolá <nicolas.ortola@bpmx.com.ar>
2020-04-18 21:56:07 -0300

    EX 3 unresolved

--- exercises.js
+++ exercises.js
@@ -20,7 +20,7 @@ const catalog = [
 
 /* Desarrollar la función addToCatalog(name, stock), que recibe un nombre (texto) y un stock (cantidad), agregue ambos datos como un único objeto al array catalog. Luego tiene que mostrar por consola la colección. 
  */
-function addToCatalog(_name, _stock) {
+function addBoxToCatalog(_name, _stock) {
     // const box = {name: _name, stock: _stock}
     catalog.push({name: _name, stock: _stock})
     console.log (catalog)
@@ -28,8 +28,8 @@ function addToCatalog(_name, _stock) {
 }
 
 /* EX1 test */
-//addToCatalog('honey', 20) 
-//addToCatalog('honeyvere', 25)
+//addBoxToCatalog('honey', 20) 
+//addBoxToCatalog('honeyvere', 25)
 
 //------------------- END EX 1 --------------------------------
 
@@ -37,16 +37,40 @@ function addToCatalog(_name, _stock) {
 
 /* Ahora el cliente empieza a mandar box armadas, siempre de a dos a la vez. Desarrollar la función addTwoContainersToCatalog() */
 
-function addTwoContainersToCatalog(container) {
+function addTwoBoxesToCatalog(container) {
     catalog.push(container[0], container[1])
 }
 
-/* EX2 test */
-const containers1 = [{name: 'menta', stock: 4}, {name: 'fruit', stock: 9}]
-const containers2 = [ {name: 'pera', stock: 7}, {name: 'chocolate', stock: 2}]
+/* EX2 test 
+const container1 = [{name: 'menta', stock: 4}, {name: 'fruit', stock: 9}]
+const container2 = [ {name: 'pera', stock: 7}, {name: 'chocolate', stock: 2}]
 
-addTwoContainersToCatalog(containers1)
-addTwoContainersToCatalog(containers2)
+addTwoBoxesToCatalog(container1)
+addTwoBoxesToCatalog(container2)
+
+console.log(catalog)*/
+//------------------- END EX 2 --------------------------------
+
+//------------------- EX 3 ------------------------------------
+
+/*Ahora el cliente manda containers de n boxes, pueden tener una, dos, tres, o más. Desarrollar
+la funcion addNContainersToCatalog() */
+
+function addNBoxesToCatalog(container){
+
+}
+
+/* EX3 test */
+const container1 = [{name: 'menta', stock: 4}, {name: 'fruit', stock: 7}, {name: 'pera', stock: 7}]
+const container2 = [{name: 'maní', stock: 2}]
+const container3 = []
+const container4 = [ {name: 'multifru', stock: 13}, {name: 'vereConAcor', stock: 2}, {name: 'nutella', stock: 4}, {name: 'pija', stock: 9}]
+
+addNBoxesToCatalog(container1)
+addNBoxesToCatalog(container2)
+addNBoxesToCatalog(container3)
+addNBoxesToCatalog(container4)
 
 console.log(catalog)
-//------------------- EX 2 --------------------------------
+
+//------------------- END EX 3 --------------------------------
